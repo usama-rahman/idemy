b5083feda74671da75ac05ba96a296c704a1e277
usama-rahman <rahmanusama390@gmail.com>
2024-08-22 14:32:28 +0600

    Model Added

---
+++ src/db/schema/category.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface ICategory extends Document {
+  title: string;
+  description?: string;
+  thumbnail: string;
+}
+
+const categorySchema: Schema<ICategory> = new Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: false,
+    type: String,
+  },
+  thumbnail: {
+    required: true,
+    type: String,
+  },
+});
+
+export const Category: Model<ICategory> =
+  mongoose.models.Category ??
+  mongoose.model<ICategory>("Category", categorySchema);
---
+++ src/db/schema/user.ts
@@ -0,0 +1,61 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+  phone?: string;
+  role: string;
+  bio?: string;
+  socialMedia?: Record<string, string>;
+  profilePicture?: string;
+  designation?: string;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  phone: {
+    required: false,
+    type: String,
+  },
+  role: {
+    required: true,
+    type: String,
+  },
+  bio: {
+    required: false,
+    type: String,
+  },
+  socialMedia: {
+    required: false,
+    type: Map,
+    of: String,
+  },
+  profilePicture: {
+    required: false,
+    type: String,
+  },
+  designation: {
+    required: false,
+    type: String,
+  },
+});
+
+export const User: Model<IUser> =
+  mongoose.models.User ?? mongoose.model<IUser>("User", userSchema);
