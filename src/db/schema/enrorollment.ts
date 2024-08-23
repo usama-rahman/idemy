4da0ffe0dc538b22c21e4685e947f15019ef8ac9
usama-rahman <rahmanusama390@gmail.com>
2024-08-23 12:56:34 +0600

    Schema Added

---
+++ src/db/schema/enrorollment.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IEnrollment extends Document {
+  enrollment_date: Date;
+  status: string;
+  completion_date?: Date;
+  method: string;
+  course: mongoose.Types.ObjectId;
+  student: mongoose.Types.ObjectId;
+}
+
+const enrollmentSchema: Schema<IEnrollment> = new Schema({
+  enrollment_date: {
+    required: true,
+    type: Date,
+  },
+  status: {
+    required: true,
+    type: String,
+  },
+  completion_date: {
+    required: false,
+    type: Date,
+  },
+  method: {
+    required: true,
+    type: String,
+  },
+  course: { type: Schema.Types.ObjectId, ref: "Course" },
+  student: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+export const Enrollment: Model<IEnrollment> =
+  mongoose.models.Enrollment ??
+  mongoose.model<IEnrollment>("Enrollment", enrollmentSchema);
---
+++ src/db/schema/quiz.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IQuiz extends Document {
+  title: string;
+  description?: string;
+  explanations?: string;
+  slug?: string;
+  options?: any[];
+  mark: number;
+}
+
+const quizzesSchema: Schema<IQuiz> = new Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  explanations: {
+    type: String,
+  },
+  slug: {
+    type: String,
+  },
+  options: {
+    type: Array,
+  },
+  mark: {
+    required: true,
+    default: 5,
+    type: Number,
+  },
+});
+
+export const Quiz: Model<IQuiz> =
+  mongoose.models.Quiz ?? mongoose.model<IQuiz>("Quiz", quizzesSchema);
