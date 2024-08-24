374e5a69b963d246039d3394640eacaf29ebe1cb
usama-rahman <rahmanusama390@gmail.com>
2024-08-24 14:38:54 +0600

    Auth.ts Added

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,34 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { User } from "./db/schema/user";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub, Google],
+  providers: [
+    Credentials({
+      async authorize(credential) {
+        if (credential == null) return null;
+
+        try {
+          const user = await User.findOne({
+            email: credential?.email,
+          });
+
+          if (user) {
+            const isMatch = await bcrypt.compare(credential.password as string, user.password);
+
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Check your password");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (err) {
+          throw new Error(err as string);
+        }
+      },
+    }),
+  ],
 });
